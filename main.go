e78db1312e48f7887858a4cc5846403f3180e2b6
Galvani Júnior <galvanidev@gmail.com>
2021-04-26 19:34:27 -0300

    init project

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"log"
+	"net"
+)
+
+func listen(con net.Conn) {
+
+	reader := bufio.NewReader(con)
+	b, _ := reader.ReadByte()
+
+	var relaycon net.Conn
+	var relayreader *bufio.Reader
+
+	switch b {
+	case 123:
+		log.Println("Relaying for delve debug")
+		relaycon, err := net.Dial("tcp", "127.0.0.1:9000")
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		log.Println("success ", relaycon)
+
+	default:
+		log.Println("Relaying for other process")
+		relaycon, err := net.Dial("tcp", "127.0.0.1:8081")
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		log.Println("success ", relaycon)
+	}
+
+	relayreader = bufio.NewReader(relaycon)
+	relaycon.Write([]byte{b})
+
+	go relaybuffer(reader, relaycon)
+	relaybuffer(relayreader, con)
+
+	relaycon.Close()
+	con.Close()
+}
+
+func relaybuffer(reader *bufio.Reader, conn net.Conn) {
+	for {
+		b, err := reader.ReadByte()
+		_, err = conn.Write([]byte{b})
+		if err != nil {
+			break
+		}
+	}
+}
+
+func main() {
+	port := flag.String("port", "5000", "listen port proxy")
+	flag.Parse()
+	server, err := net.Listen("tcp", ":"+*port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		con, _ := server.Accept()
+		go listen(con)
+	}
+}
